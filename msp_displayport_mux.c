e3b431fd23998becb8050ce4f79f01838d67c81e
bri3d <brian@brianledbetter.com>
2022-03-31 09:12:26 -0600

    don't busyloop in mux

--- msp_displayport_mux.c
+++ msp_displayport_mux.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/poll.h>
 
 #include "network.h"
 #include "serial.h"
@@ -37,16 +38,22 @@ static void msp_callback(msp_msg_t *msp_message)
 
 int main() {
     signal(SIGINT, sig_handler);
+    struct pollfd poll_fds[2];
     const char *pty_name_ptr;
     msp_state_t *msp_state = calloc(1, sizeof(msp_state_t));
     msp_state->cb = &msp_callback;
     serial_fd = open_serial_port(SERIAL_PORT);
     pty_fd = open_pty(&pty_name_ptr);
-    printf("%s", pty_name_ptr);
+    printf("Allocated PTY %s\n", pty_name_ptr);
     socket_fd = connect_to_server(IP_ADDRESS, PORT);
     uint8_t serial_byte = 0;
     uint8_t pty_byte = 0;
     while (!quit) {
+        poll_fds[0].fd = serial_fd;
+        poll_fds[1].fd = pty_fd;
+        poll_fds[0].events = POLLIN;
+        poll_fds[1].events = POLLIN;
+        poll(poll_fds, 2, -1); 
         if (read(serial_fd, &serial_byte, 1) > 0) {
             if(msp_process_data(msp_state, serial_byte) == 0) {
                 // 0 -> MSP data was valid, so buffer it to forward on later
@@ -59,6 +66,7 @@ int main() {
         if(read(pty_fd, &pty_byte, 1) > 0) {
            write(serial_fd, &pty_byte, 1);
         }
+
     }
     close(serial_fd);
     close(pty_fd);
