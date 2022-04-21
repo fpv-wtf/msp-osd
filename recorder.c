8f88f85b3c64b8b1d04508515c1e5cf43f59e2c2
bri3d <brian@brianledbetter.com>
2022-04-20 19:04:27 -0600

    switch recorder to async IO

--- recorder.c
+++ recorder.c
@@ -5,27 +5,53 @@
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <errno.h>
 
+#include "aio.h"
 #include "dji_media.h"
 
+typedef struct frame_io_cb_s {
+    struct iocb *cb;
+    void *data;
+    int len;
+} frame_io_cb_t;
+
 static volatile uint8_t quit = 0;
+
+int transfer_channel = 0;
 
 static void sig_handler(int _)
 {
     quit = 1;
 }
 
+void release_frame(frame_io_cb_t *frame_info) {
+    free(frame_info->cb);
+    free(frame_info->data);
+    free(frame_info);
+}
+
 int main(int argc, char *argv[]) {
     signal(SIGINT, sig_handler);
+
+    struct io_event events[1];
 
     duss_media_apcp_cmd_t apcp_cmd;
     bridge_io_pkt_t pkt;
 
     int media_channel = dji_open_media_control_channel();
-    int transfer_channel = dji_open_video_transfer_channel();
+    transfer_channel = dji_open_video_transfer_channel();
     FILE *out_file = fopen(argv[1], "wb");
+    static uint64_t file_offset = 0;
+
+    aio_context_t ctx;
+    memset(&ctx, 0, sizeof(ctx));
+    io_setup(64, &ctx);
 
     dji_send_media_command(media_channel, DUSS_MEDIA_CMD_GND_STO_START);
+
+    uint32_t last_frame_counter = 0;
+    uint32_t frame_counter = 0;
 
     while(!quit) {
         if(ioctl(transfer_channel, DUSS_CLAIM_BRIDGE_IO_PKT, &pkt) == 0) {
@@ -53,17 +79,50 @@ int main(int argc, char *argv[]) {
             }
             default:
             {
+                struct iocb *cb = calloc(1, sizeof(struct iocb));
+                struct iocb *list_of_iocb[1] = {cb};
+
+                frame_io_cb_t *frame_io = calloc(1, sizeof(frame_io_cb_t));
+                
                 void *frame_data = (void *)(frame_base_addr + *(int *)(frame_base_addr + 0xc));
+                last_frame_counter = frame_counter;
+                frame_counter = *(uint32_t *)(frame_base_addr + 0x18);
+                if(last_frame_counter + 1 != frame_counter) {
+                    printf("!!! Frame counter mismatch, wanted %d got %d\n", last_frame_counter + 1, frame_counter);
+                }
+
                 uint32_t frame_len = *(uint32_t *)(frame_base_addr + 0x10);
-                printf("got %p data len %d\n", frame_data, frame_len);
-                fwrite(frame_data, frame_len, 1, out_file);
+                
+                printf("got %p data len %d frame counter %d\n", frame_data, frame_len, frame_counter);
+                
+                void *frame_data_copy = malloc(frame_len);
+                memcpy(frame_data_copy, frame_data, frame_len);
+                
+                munmap(frame, size);
+                ioctl(transfer_channel, DUSS_RELEASE_BRIDGE_IO_PKT, &pkt);
+               
+                frame_io->cb = cb;
+                frame_io->data = frame_data_copy;
+                frame_io->len = frame_len;
+                cb->aio_buf = (uintptr_t)frame_data_copy;
+                cb->aio_fildes = fileno(out_file);
+                cb->aio_nbytes = frame_len;
+                cb->aio_offset = file_offset;
+                cb->aio_lio_opcode = IOCB_CMD_PWRITE;
+                cb->aio_data = (uintptr_t)frame_io;
+                int ret = io_submit(ctx, 1, list_of_iocb);  
+                file_offset += frame_len; 
                 break;
             }
             }
-            munmap(frame, size);
-            ioctl(transfer_channel, DUSS_RELEASE_BRIDGE_IO_PKT, &pkt);
         }
-        usleep(5000);
+        if(io_getevents(ctx, 0, 1, events, 0) > 0) {
+            struct io_event *ev = &events[0];
+            frame_io_cb_t *frame_io = (frame_io_cb_t *)ev->data;
+            printf("Frame IO cb wrote data, releasing buffers: %p : %d\n", frame_io->data, frame_io->len);
+            release_frame(frame_io);
+        }
+        usleep(1500);
     }
     dji_send_media_command(media_channel, DUSS_MEDIA_CMD_GND_STO_STOP);
     close(transfer_channel);
