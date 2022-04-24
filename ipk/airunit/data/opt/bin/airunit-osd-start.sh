039e8917f9f50ac113a8ea64b2a695baf39e4196
Chris L <stylesuxx@gmail.com>
2022-04-24 19:28:18 +0200

    Reconfigured so that on the goggle the process is run as background process and dinit can properly monitor it by the muxers PID

--- ipk/airunit/data/opt/bin/airunit-osd-start.sh
+++ ipk/airunit/data/opt/bin/airunit-osd-start.sh
@@ -1,8 +1,11 @@
 #!/system/bin/sh
+setprop dji.hdvt_uav_service 0
+
 if [ ! -e "/dev/ttyS1_moved" ]
 then
-  setprop dji.hdvt_uav_service 0
   mv /dev/ttyS1 /dev/ttyS1_moved
-  /opt/bin/msp_displayport_mux 192.168.41.2 /dev/ttyS1_moved /dev/ttyS1 &
-  setprop dji.hdvt_uav_service 1
 fi
+
+/opt/bin/msp_displayport_mux 192.168.41.2 /dev/ttyS1_moved /dev/ttyS1 &
+echo $! > /opt/var/run/airunit-osd-dji.pid
+setprop dji.hdvt_uav_service 1
