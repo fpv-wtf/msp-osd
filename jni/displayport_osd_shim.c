773eba3af3a68e9d0a2ddabe9851b0108d3946dc
bri3d <brian@brianledbetter.com>
2022-08-20 11:56:49 -0600

    steal display driver using acquire plane instead of calloc

--- jni/displayport_osd_shim.c
+++ jni/displayport_osd_shim.c
@@ -7,13 +7,8 @@
 #include "osd.h"
 #include "hw/dji_display.h"
 
-typedef struct dfb_fbdev_s {
-    void *hal_device;
-    duss_disp_instance_handle_t *disp_instance;
-} dfb_fbdev_t;
-
-static dfb_fbdev_t *dfb_fbdev = 0;
-static duss_hal_obj_handle_t ion_handle = 0;
+static duss_disp_instance_handle_t *disp_instance = NULL;
+static duss_hal_obj_handle_t ion_handle = NULL;
 static int started = 0;
 
 // Patch a seemingly unused window management thread in libtp2801.so to inject our code into the dji_glasses process.
@@ -21,25 +16,28 @@ static int started = 0;
 void _ZN24MMSFBWindowManagerThread10threadMainEv(void *this) {
     printf("ENTERING MAIN \n");
     while(1) {
-        if(started == 0 && dfb_fbdev != NULL && ion_handle != NULL) {
-            printf("ENTERING OSD! fbdev ptr %x -> disp %x ion %x\n", dfb_fbdev, dfb_fbdev->disp_instance, ion_handle);
+        if(started == 0 && disp_instance != NULL && ion_handle != NULL) {
+            printf("ENTERING OSD! fbdev disp %x ion %x\n", disp_instance, ion_handle);
             started = 1;
-            osd_directfb(dfb_fbdev->disp_instance, ion_handle);
+            osd_directfb(disp_instance, ion_handle);
         }
         usleep(50000);
     }
 }
 
-duss_result_t (*duss_hal_mem_alloc2)(duss_hal_obj_handle_t handle, duss_hal_mem_handle_t *mem_handle, uint32_t size, uint32_t param1, uint32_t param2, uint32_t param3) = 0;
-static void *hal_lib = 0;
+static void *hal_lib = NULL;
 
+static duss_result_t (*duss_hal_mem_alloc2)(duss_hal_obj_handle_t handle, duss_hal_mem_handle_t *mem_handle, uint32_t size, uint32_t param1, uint32_t param2, uint32_t param3) = 0;
 // Patch libduml_hal's duss_hal_mem_alloc.
 // Use the first invocation of this to steal the duss_hal_obj_handle_t pointing to the ion shared memory service.
 duss_result_t duss_hal_mem_alloc(duss_hal_obj_handle_t handle, duss_hal_mem_handle_t *mem_handle, uint32_t size, uint32_t param1, uint32_t param2, uint32_t param3) {
     if (duss_hal_mem_alloc2 == NULL) {
 	    duss_hal_mem_alloc2 = dlsym (RTLD_NEXT, "duss_hal_mem_alloc");
 		if (duss_hal_mem_alloc2 == 0){
-			hal_lib = dlopen("/system/lib/libduml_hal.so", 1);
+			if(hal_lib == NULL)
+			{
+				hal_lib = dlopen("/system/lib/libduml_hal.so", 1);
+			}
 			duss_hal_mem_alloc2 = dlsym (hal_lib, "duss_hal_mem_alloc");
 			if (duss_hal_mem_alloc2 == 0)
 			{
@@ -54,30 +52,28 @@ duss_result_t duss_hal_mem_alloc(duss_hal_obj_handle_t handle, duss_hal_mem_hand
 	return duss_hal_mem_alloc2(handle, mem_handle, size, param1, param2, param3);
 }
 
+static duss_result_t (*duss_hal_display_aquire_plane2)(duss_disp_instance_handle_t * , duss_disp_plane_type_t , duss_disp_plane_id_t * ) = 0;
 
-static void *(*calloc2) ( size_t count, size_t bytes) = 0;
-static void *direct_lib = 0;
-
-// Patch libdirect.so's calloc. Detect the allocation of a dfb_fbdev object (first allocated struct with size 0x4C).
-// Use this struct to steal the pointer to the duss_disp_instance_handle_t.
-void *direct_calloc ( size_t count, size_t bytes) {
-    if(calloc2 == NULL) {
-	    calloc2 = dlsym (RTLD_NEXT, "direct_calloc");
-		if (calloc2 == 0){
-			direct_lib = dlopen("/system/lib/libdirect.so", 1);
-			calloc2 = dlsym (direct_lib, "direct_calloc");
-			if (calloc2 == 0)
+// Patch libduml_hal's duss_hal_display_aquire_plane.
+// Use the first invocation of this with plane == 5 to steal the duss_hal_instance_handle_t pointing to the display driver.
+duss_result_t duss_hal_display_aquire_plane(duss_disp_instance_handle_t *disp, duss_disp_plane_type_t plane_type, duss_disp_plane_id_t *plane_id) {
+	if (duss_hal_display_aquire_plane2 == NULL) {
+	    duss_hal_display_aquire_plane2 = dlsym (RTLD_NEXT, "duss_hal_display_aquire_plane");
+		if (duss_hal_display_aquire_plane2 == 0){
+			if(hal_lib == NULL)
+			{
+				hal_lib = dlopen("/system/lib/libduml_hal.so", 1);
+			}
+			duss_hal_display_aquire_plane2 = dlsym (hal_lib, "duss_hal_display_aquire_plane");
+			if (duss_hal_display_aquire_plane2 == 0)
 			{
 				printf("dlsym: %s\n", dlerror());
 				return -1;
 			}
 		}
     }
-	void *cal = calloc2(count, bytes);
-	
-	if(bytes == 0x4c && dfb_fbdev == NULL) {  // dfb_fbdev
-		dfb_fbdev = cal;
+	if(disp_instance == NULL && *plane_id == 5) {
+		disp_instance = disp;
 	}
-	
-	return cal;
+	return duss_hal_display_aquire_plane2(disp, plane_type, plane_id);
 }
