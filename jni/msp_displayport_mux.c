c2734a6846a601d4a50f446d5e882bb56f625c1f
bri3d <brian@brianledbetter.com>
2022-09-26 14:09:30 -0600

    we do need to poll for variant after all

--- jni/msp_displayport_mux.c
+++ jni/msp_displayport_mux.c
@@ -124,8 +124,10 @@ static void rx_msp_callback(msp_msg_t *msp_message)
         }
     } else {
         uint16_t size = msp_data_from_msg(message_buffer, msp_message);
+        // This is an FC Variant response, so we want to use it to set our FC variant.
         if(msp_message->cmd == MSP_CMD_FC_VARIANT) {
-            memcpy(current_fc_identifier, message_buffer, 4);
+            DEBUG_PRINT("Got FC Variant response!\n");
+            memcpy(current_fc_identifier, msp_message->payload, 4);
         }
         // This isn't an MSP DisplayPort message, so send it to either DJI directly or to the cache.
         if(serial_passthrough) {
@@ -169,6 +171,12 @@ static void tx_msp_callback(msp_msg_t *msp_message)
     }
 }
 
+static void send_variant_request(int serial_fd) {
+    uint8_t buffer[255];
+    construct_msp_command(buffer, MSP_CMD_FC_VARIANT, NULL, 0, MSP_OUTBOUND);
+    write(serial_fd, &buffer, 6);
+}
+
 static void send_data_packet(int data_socket_fd, dji_shm_state_t *dji_shm) {
     packet_data_t data;
     memset(&data, 0, sizeof(data));
@@ -176,7 +184,7 @@ static void send_data_packet(int data_socket_fd, dji_shm_state_t *dji_shm) {
     data.tx_temperature = get_int_from_fs(CPU_TEMP_PATH);
     data.tx_voltage = get_int_from_fs(AU_VOLTAGE_PATH);
     memcpy(data.fc_variant, current_fc_identifier, sizeof(current_fc_identifier));
-    DEBUG_PRINT("got bitrate %f Mbit voltage %f V temp %d C variant %.4s\n", (float)(data.tx_bitrate / 1000.0f), (float)(data.tx_voltage / 64.0f), data.tx_temperature, data.fc_variant);
+    DEBUG_PRINT("got voltage %f V temp %d C variant %.4s\n", (float)(data.tx_voltage / 64.0f), data.tx_temperature, data.fc_variant);
     write(data_socket_fd, &data, sizeof(data));
 }
 
@@ -291,6 +299,9 @@ int main(int argc, char *argv[]) {
             // More than 500ms have elapsed, let's go ahead and send a data frame
             clock_gettime(CLOCK_MONOTONIC, &last);
             send_data_packet(data_fd, &dji_radio);
+            if(current_fc_identifier[0] == 0) {
+                send_variant_request(serial_fd);
+            }
         }
     }
     close_dji_radio_shm(&dji_radio);
